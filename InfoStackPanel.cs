c8cf4443ea9d526e13b43c08d2ffbb7c0b495e61
SaintAllary <49682381+SaintAllary@users.noreply.github.com>
2020-09-22 22:37:29 +0300

    Created left bar

---
+++ InfoStackPanel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RuslanMessager
+{
+    class InfoStackPanel : StackPanel
+    {
+        public InfoStackPanel()
+        {
+            
+            //Background = Brushes.Blue;
+            //Children.Add(new System.Windows.Controls.Button()
+            //{BorderThickness=Thickness(0)
+            //,Height = 62,Content
+        //} );
+          
+        }
+    }
+}
