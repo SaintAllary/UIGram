fb128e0771e45ca33becce2a3018e0defc55feb8
SaintAllary <49682381+SaintAllary@users.noreply.github.com>
2020-09-24 23:59:07 +0300

    Last Commit 24

---
+++ IMessage.cs
@@ -0,0 +1,32 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuslanMessager
+{
+    interface IMessage
+    {
+        /// <summary>
+        /// Can contain url on any media
+        /// </summary>
+        string MessageContentUrl { get; set; }
+        /// <summary>
+        /// Contain full data
+        /// </summary>
+        string SendDateTime { get; set; }
+        string SenderName { get; set; }
+        string MessageText { get; set; }
+        /// <summary>
+        /// Show does message read
+        /// </summary>
+        bool DoesRead { get; set; }
+        /// <summary>
+        /// Show 
+        /// </summary>
+        bool MyTurn { get; set; }
+
+    }
+}
