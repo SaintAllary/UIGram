0fd355a14e667617a062ca7f1e9c293bc751eefd
Usagi <kirito232k@gmail.com>
2020-09-26 21:33:50 +0300

    BaseMsgSend

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,31 +48,27 @@ namespace RuslanMessager
         }
 
         private void MainWindow1_SizeChanged(object sender, SizeChangedEventArgs e) {
-     
+
             if (MainWindow1.RenderSize.Width < 815)
                 ResizeColoum(3, 0, 0, GridUnitType.Pixel);
             else if (MainWindowGrid.ColumnDefinitions[3].MinWidth == 0 && MainWindow1.RenderSize.Width > 815)
-                ResizeColoum(3,380,1,GridUnitType.Star);
-  
-        }
-
-        private void ColorZone_Loaded(object sender, RoutedEventArgs e)
-        {
+                ResizeColoum(3, 380, 1, GridUnitType.Star);
 
         }
-        private void ResizeColoum(int indexPosition,double minWidth,double value, GridUnitType gridUnitType)
-        {
+
+        private void ColorZone_Loaded(object sender, RoutedEventArgs e) {
+
+        }
+        private void ResizeColoum(int indexPosition, double minWidth, double value, GridUnitType gridUnitType) {
             MainWindowGrid.ColumnDefinitions[indexPosition].Width = new GridLength(1, gridUnitType);
             MainWindowGrid.ColumnDefinitions[indexPosition].MinWidth = minWidth;
         }
 
-        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
-        {
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e) {
 
         }
 
-        private void AddUserButton_Click(object sender, RoutedEventArgs e)
-        {
+        private void AddUserButton_Click(object sender, RoutedEventArgs e) {
             PreviewInfoSerializable preview = new PreviewInfoSerializable();
 
             AddUserDialog addUserDialog = new AddUserDialog();
@@ -80,19 +76,25 @@ namespace RuslanMessager
 
             preview.UserName = addUserDialog.NameTextBox.Text;
             preview.PhoneNumber = addUserDialog.NumberTextBox.Text;
-         
 
-            if (addUserDialog.DoexExecuted == true)
-            {
+
+            if (addUserDialog.DoexExecuted == true) {
                 PreviewsPanel.Children.Add(new UserDialogPreviewButton(preview.UserName) { });
-            }                 
-            
+            }
+
         }
 
         private void FastAddUserBtn_Click(object sender, RoutedEventArgs e) {
             PreviewsPanel.Children.Add(new UserDialogPreviewButton("TEST USER") { });
             PreviewsPanel.Children.Add(new UserDialogPreviewButton("TEST USER") { });
             PreviewsPanel.Children.Add(new UserDialogPreviewButton("TEST USER") { });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var msg = new MessageUiForm(this.MyMsg.Text);
+            //msg.MessageText = ;
+            
+            this.MessageListBox.Items.Add(msg);
         }
     }
 }
--- MessageUiForm.cs
+++ MessageUiForm.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -16,12 +17,18 @@ namespace RuslanMessager
         FormattedText MessageTextFormatted;
         TextBlock textBlock;
         Card msgCard;
+        PackIcon packIcon;
+        StackPanel panel;
 
         [Obsolete]
-        public MessageUiForm() : base() {
+        public MessageUiForm(string _messageText, bool IsMyMsg = true) : base() {
+            packIcon = new PackIcon();
+            myTurn = IsMyMsg;
             msgCard = new Card();
 
-            MessageText = "SOME TEXT new line aaa AAAAAAABAaaaaaaaaaaaaaaaaaaBBBBBBBBBBBBBBBBaaaaaaaaaaaaaaaaaaaaaaaA\n\n\n\naaa";
+            MessageText = string.Empty;
+            MessageText = _messageText;
+
             MessageTextFormatted = new FormattedText(MessageText, CultureInfo.CurrentCulture, System.Windows.FlowDirection.LeftToRight, new Typeface("Calibri"), 12, System.Windows.Media.Brushes.Black);
 
             textBlock = new TextBlock();
@@ -38,19 +45,32 @@ namespace RuslanMessager
             msgCard.FontSize = 12;
             msgCard.FontFamily = new System.Windows.Media.FontFamily("Calibri");
 
+            ChooseAlignment();
 
             msgCard.Content = textBlock;
 
-            ChooseAlignment();
+            panel = new StackPanel();
+            panel.Children.Add(msgCard);
 
-            this.AddChild(msgCard);
-            this.Background = Brushes.Transparent;
+            
+            packIcon.Kind = PackIconKind.NetworkStrength4;
+            packIcon.Width = 10;
+            packIcon.Height = 10;
+            packIcon.Margin = new System.Windows.Thickness(-4, -9, 0, 0);
+            packIcon.Foreground = Brushes.White;           
+
+            panel.Children.Add(packIcon);
+
+            this.AddChild(panel);
+            this.Background = Brushes.Transparent;           
         }
 
         private void ChooseAlignment() {
             if (MyTurn) {
                 this.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
                 msgCard.Background = new SolidColorBrush(Color.FromRgb(239, 253, 222));
+                packIcon.Foreground = new SolidColorBrush(Color.FromRgb(239, 253, 222));
+                //(packIcon.RenderTransform).((ScaleTransform)packIcon.RenderTransform).ScaleX = -1;
             }
             else
                 msgCard.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
